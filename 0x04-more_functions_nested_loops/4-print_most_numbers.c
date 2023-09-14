30008020fa8add5189d3108c7e091aff49b1d8cd
Reda <redoxraja2003@gmail.com>
2023-09-14 22:18:24 +0100

    1-last_digit.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  *print_most_numbers - prints numbers except 2 and 4
+ *
  *Return: returns nothing
 */
 void print_most_numbers(void)
@@ -10,7 +11,7 @@ void print_most_numbers(void)
 	for (number = 48; number < 58; number++)
 {
 	if ((number == 50) || (number == 52))
-
+{
 	continue;
 }
 	_putchar(number);
