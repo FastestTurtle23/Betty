3cabbc43dbbc86f87c1bf5a5f49df85ce454e2d7
Reda <redoxraja2003@gmail.com>
2023-09-14 04:13:21 +0100

    1-last_digit.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,17 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * print_alphabet_x10 - repeats the print_alphabet 10 times.
- * Return: Nothing.
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
 */
 void print_alphabet_x10(void)
 {
-	int i;
-	char c;
+	int count = 0;
+	char letter;
 
-	for (i = 0; i < 10; ++i)
+	while (count++ <= 9)
 {
-	for (c = 'a'; c <= 'z'; c++)
-{
-	_putchar(c);
-}
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
 	_putchar('\n');
 }
 }
